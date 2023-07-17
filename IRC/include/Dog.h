c73f5ec5ea229f865b8d8478d04391d1e0b012b2
Julian Schneider <jtschneider99@icloud.com>
2023-07-17 15:34:55 +0200

    Initial commit

---
+++ IRC/include/Dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_HPP
+# define DOG_HPP
+
+// Includes
+
+class Dog
+{
+	public:
+		// Constructors
+		Dog();
+		Dog(const Dog &copy);
+
+		// Destructor
+		~Dog();
+
+		// Operators
+		Dog & operator=(const Dog &assign);
+
+		// Getters / Setters
+
+		// Member functions
+
+	private:
+
+};
+
+#endif
